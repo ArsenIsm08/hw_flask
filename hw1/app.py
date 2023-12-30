abdd04fbebacb1543d9e423328d76c9a956105bd
Ars <ismailovsenya@gmail.com>
2023-12-30 21:51:01 +0600

    delete_attribyte_action

--- hw1/app.py
+++ hw1/app.py
@@ -3,36 +3,31 @@ from flask import Flask, render_template, request, redirect, make_response
 app = Flask(__name__, template_folder='templates')
 app.secret_key = 'Arsen'
 
-@app.route('/')
-def input_form():
-    return render_template('input_form.html')
+@app.route('/', methods=['GET', 'POST'])
+def handle_form():
+    if request.method == 'POST':
+        user_name = request.form.get('user_name')
+        user_email = request.form.get('user_email')
 
-@app.route('/process_form', methods=['POST'])
-def process_form():
-    user_name = request.form.get('user_name')
-    user_email = request.form.get('user_email')
+        response = make_response(redirect('/welcome'))
+        response.set_cookie('user_name', user_name)
+        response.set_cookie('user_email', user_email)
 
-    response = make_response(redirect('/welcome'))
-    response.set_cookie('user_name', user_name)
-    response.set_cookie('user_email', user_email)
+        return response
 
-    return response
-
-@app.route('/welcome')
-def welcome():
     user_name = request.cookies.get('user_name')
-    if user_name:
+
+    if request.path == '/welcome' and user_name:
         return render_template('welcome.html', user_name=user_name)
-    else:
-        return redirect('/')
 
-@app.route('/logout')
-def logout():
-    response = make_response(redirect('/'))
-    response.delete_cookie('user_name')
-    response.delete_cookie('user_email')
+    if request.path == '/logout':
+        response = make_response(redirect('/'))
+        response.delete_cookie('user_name')
+        response.delete_cookie('user_email')
 
-    return response
+        return response
+
+    return render_template('input_form.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
